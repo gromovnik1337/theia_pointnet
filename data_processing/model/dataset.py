2d694de65e892e061fc4bbcf030c5fe6e1f8aaea
Vice <vice.roncevic@gmail.com>
2023-09-23 14:12:22 +0200

    Finalized the basic structure of the model

--- data_processing/model/dataset.py
+++ data_processing/model/dataset.py
@@ -20,6 +20,7 @@ from typing import Dict
 
 SEED = 42
 
+
 class SamplePc(object):
     def __init__(self, points: int = 1024):
         """
@@ -27,91 +28,100 @@ class SamplePc(object):
             points: Number of points to sample from the mesh.
         """
         self.points = points
+
     def __call__(self, mesh: trimesh.Trimesh):
         """Samples the point cloud from the mesh surface.
-        
+
         Args:
             mesh: Input mesh.
         Returns:
             Sampled point cloud.
         """
         pc_sampled, _ = trimesh.sample.sample_surface(mesh, self.points, seed=SEED)
-    
+
         return pc_sampled
-    
+
 
 class NormalizePc(object):
-    def __call__(self, pc:np.ndarray)-> np.ndarray:
+    def __call__(self, pc: np.ndarray) -> np.ndarray:
         """Normalizes point cloud to a unit sphere.
-        
+
         Args:
             pc: Input point cloud.
-        
+
         Returns:
             Normalized point cloud.
         """
         if len(pc.shape) != 2:
-            print('Invalid point cloud!')
+            print("Invalid point cloud!")
             return np.array([])
         pc_norm = pc - np.mean(pc, axis=0)
         pc_norm /= np.max(np.linalg.norm(pc_norm, axis=1))
-        
+
         return pc_norm
-    
+
+
 class ApplyRandomRotationZ(object):
-    def __call__(self, pc:np.ndarray) -> np.ndarray:
+    def __call__(self, pc: np.ndarray) -> np.ndarray:
         """Applies random rotation around the z axis
         to the input point cloud.
-    
+
         Args:
             pc: Input point cloud.
-    
+
         Returns:
             Rotated point cloud.
         """
         if len(pc.shape) != 2:
-            print('Invalid point cloud!')
+            print("Invalid point cloud!")
             return np.array([])
         theta = np.random.random(1) * 2 * math.pi
-        rot_matrix = np.array([[math.cos(theta), -math.sin(theta), 0],
-                               [math.sin(theta), math.cos(theta), 0],
-                               [0, 0, 1]])
-          
+        rot_matrix = np.array(
+            [
+                [math.cos(theta), -math.sin(theta), 0],
+                [math.sin(theta), math.cos(theta), 0],
+                [0, 0, 1],
+            ]
+        )
+
         pc_rot = rot_matrix.dot(pc.T).T
-    
+
         return pc_rot
-    
+
+
 class AddJitter(object):
-    def __call__(self, pc:np.ndarray) -> np.ndarray:
+    def __call__(self, pc: np.ndarray) -> np.ndarray:
         """Applied random jitter to the point cloud.
-    
+
         Args:
             pc: Input point cloud.
-    
+
         Returns:
             Point cloud with added noise.
         """
         if len(pc.shape) != 2:
-            print('Invalid point cloud!')
+            print("Invalid point cloud!")
             return np.array([])
         jitter = np.random.normal(0, 0.02, (pc.shape))
         pc_noisy = pc + jitter
-        
+
         return pc_noisy
-    
-DEFAULT_TRANSFORMS = transforms.Compose([
-                                NormalizePc()
-                               ])
+
+
+DEFAULT_TRANSFORMS = transforms.Compose([NormalizePc()])
 
 
 class McbData(Dataset):
-    def __init__(self, dataset_dir: Union[pathlib.Path, str],
-                 transforms: Type[transforms.transforms.Compose] = DEFAULT_TRANSFORMS,
-                 sampling: int = 1024):
+    def __init__(
+        self,
+        dataset_dir: Union[pathlib.Path, str],
+        transforms: Type[transforms.transforms.Compose] = DEFAULT_TRANSFORMS,
+        sampling: int = 1024,
+    ):
         """Performs initial loading of the MCB dataset, as a collection
         of mesh paths and their categories. Also, it loads the transformations
         that are to be applied on each data entry.
-        
+
         Args:
             dataset_dir: Input directory.
             transforms: A composition of transformations that are to be applied
@@ -122,19 +132,19 @@ class McbData(Dataset):
         self.dataset_dir = pathlib.Path(dataset_dir)
         self.transforms = transforms
         self.sampling = sampling
-        
+
         # Create a dict with class names & their indices.
         folders = [dir.stem for dir in sorted(dataset_dir.iterdir()) if dir.is_dir()]
         self.classes = {folder: i for i, folder in enumerate(folders)}
         # Load all the samples paths and their category idx.
         self.samples = []
         for category, category_idx in self.classes.items():
-            cat_dir = self.dataset_dir/pathlib.Path(category)
+            cat_dir = self.dataset_dir / pathlib.Path(category)
             for mesh in cat_dir.iterdir():
-                if mesh.is_file() and mesh.suffix == '.obj':
+                if mesh.is_file() and mesh.suffix == ".obj":
                     sample = {}
-                    sample['mesh_path'] = mesh.absolute()
-                    sample['category_idx'] = category_idx
+                    sample["mesh_path"] = mesh.absolute()
+                    sample["category_idx"] = category_idx
                     self.samples.append(sample)
 
     def __len__(self):
@@ -148,21 +158,20 @@ class McbData(Dataset):
     def __getitem__(self, idx: int) -> Dict[torch.tensor, int]:
         """Returns a single entry of the dataset which consists of input point
         cloud and the output category.
-    
+
         Args:
             idx: Index of the entry.
-    
+
         Returns:
             Tuple containing dataset entry.
         """
         # Load the mesh & sample the point cloud out of it.
-        mesh_path = self.samples[idx]['mesh_path']
-        category_idx = self.samples[idx]['category_idx']
-        mesh = trimesh.load(mesh_path, force='mesh')
-        pc = SamplePc(self.sampling)(mesh) 
+        mesh_path = self.samples[idx]["mesh_path"]
+        category_idx = self.samples[idx]["category_idx"]
+        mesh = trimesh.load(mesh_path, force="mesh")
+        pc = SamplePc(self.sampling)(mesh)
         # Apply transformations:
         pc = self.__preproc__(pc)
         # Create Pytorch tensor output.
         pc = torch.from_numpy(pc)
-        return {'pc': pc, 
-               'category_idx': category_idx}
+        return {"pc": pc, "category_idx": category_idx}
--- data_processing/model/model.py
+++ data_processing/model/model.py
@@ -75,46 +75,206 @@ def instantiate_regression_model(
 
     return model, criterion, optimizer
 
+
+
+
+
+
 class Tnet(torch.nn.Module):
-    """T-net model, designed to predict canonical orientation
-    of each point cloud, resembles larger PointNet.
+    """T-net model, designed to predict transformation to the canonical orientation for each
+    input point cloud or the input feature vector. It aims to achieve that final predicitons are
+    invariant under transformation. Architecture resembles larger PointNet.
     """
-    def __init__(self, k=3):
-      """_summary_
-
-      Args:
-          k (int, optional): _description_. Defaults to 3.
-            """
+    def __init__(self, k: int = 3):
+        """
+        Args:
+            k: Defines the input vector dimension.
+        """
         super().__init__()
-        self.k=k
-        self.conv1 = torch.nn.Conv1d(k,64,1)
-        self.conv2 = torch.nn.Conv1d(64,128,1)
-        self.conv3 = torch.nn.Conv1d(128,1024,1)
-        self.fc1 = torch.nn.Linear(1024,512)
-        self.fc2 = torch.nn.Linear(512,256)
-        self.fc3 = torch.nn.Linear(256,k*k)
-
+        self.k = k
+        # Define convolutional layers.
+        self.conv1 = torch.nn.Conv1d(self.k, 64, 1)
+        self.conv2 = torch.nn.Conv1d(64, 128, 1)
+        self.conv3 = torch.nn.Conv1d(128, 1024, 1)
+        # Define fully connected layers (MLP).
+        self.fc1 = torch.nn.Linear(1024, 512)
+        self.fc2 = torch.nn.Linear(512, 256)
+        self.fc3 = torch.nn.Linear(256, self.k * self.k)
+        # Define batch normalizations.
         self.bn1 = torch.nn.BatchNorm1d(64)
         self.bn2 = torch.nn.BatchNorm1d(128)
         self.bn3 = torch.nn.BatchNorm1d(1024)
         self.bn4 = torch.nn.BatchNorm1d(512)
         self.bn5 = torch.nn.BatchNorm1d(256)
-      
+            
 
-  def forward(self, input):
-    # input.shape == (bs,n,3)
-    bs = input.size(0)
-    xb = F.relu(self.bn1(self.conv1(input)))
-    xb = F.relu(self.bn2(self.conv2(xb)))
-    xb = F.relu(self.bn3(self.conv3(xb)))
-    pool = torch.nn.MaxPool1d(xb.size(-1))(xb)
-    flat = torch.nn.Flatten(1)(pool)
-    xb = F.relu(self.bn4(self.fc1(flat)))
-    xb = F.relu(self.bn5(self.fc2(xb)))
+    def forward(self, X: torch.tensor) -> torch.tensor:
+        """Defines 1 forward pass of the T-net.
+        Output transformation is generated from the elements of the last vector, which is the
+        output of the last fully connected layer.
+
+        Args:
+            X: Input data.
+
+        Returns:
+            Predicted transformation.
+        """
+        # Pass the input through conv layers with ReLU activation.
+        xb = F.relu(self.bn1(self.conv1(X)))
+        xb = F.relu(self.bn2(self.conv2(xb)))
+        xb = F.relu(self.bn3(self.conv3(xb)))
+        # Apply the symmetrical function.
+        pool = torch.nn.MaxPool1d(xb.size(-1))(xb)
+        # Pass through fully connected layers.
+        flat = torch.nn.Flatten(1)(pool)
+        xb = F.relu(self.bn4(self.fc1(flat)))
+        xb = F.relu(self.bn5(self.fc2(xb)))
+
+        # Initialize transformation matrix as identity matrix.
+        batch_size = X.size(0)
+        init = torch.eye(self.k, requires_grad = True).repeat(batch_size, 1, 1)
+        if xb.is_cuda:
+            init = init.cuda()
+
+        # Create the output transformation from the output of the last fully connected layer.
+        xb = self.fc3(xb)
+        transformation = xb.view(-1, self.k, self.k) + init
+
+        return transformation
     
-    #initialize as identity
-    init = torch.eye(self.k, requires_grad=True).repeat(bs,1,1)
-    if xb.is_cuda:
-      init=init.cuda()
-    matrix = self.fc3(xb).view(-1,self.k,self.k) + init
-    return matrix
+class Transform(torch.nn.Module):
+    """Defines first portion of the PointNet architecture.
+    Wraps the inference of the two transformations matrices, one for the input point cloud and
+    one for the feature vector.
+
+    N.B. Output feature vector and transformation matrices are given separatelt in the output
+    as they are all required to compute the loss.
+    """
+    def __init__(self):
+        super().__init__()
+        self.t_net_input = Tnet(k=3)
+        self.t_net_feature = Tnet(k=64)
+        
+        # Define convolutional layers.
+        self.conv1 = torch.nn.Conv1d(3,64,1)
+        self.conv2 = torch.nn.Conv1d(64,128,1)
+        self.conv3 = torch.nn.Conv1d(128,1024,1)
+        # Define batch normalizations.
+        self.bn1 = torch.nn.BatchNorm1d(64)
+        self.bn2 = torch.nn.BatchNorm1d(128)
+        self.bn3 = torch.nn.BatchNorm1d(1024)
+      
+    def forward(self, X: torch.tensor) -> torch.tensor:
+        """Defines 1 forward pass of the wrapped transformation network.
+
+        Args:
+            X: Input data.
+
+        Returns:
+            Global feature vector and transformation matrices.
+        """        
+        # Transform the input point cloud using batch matrix multiplication. 
+        input_transform = self.t_net_input(X)
+        xb = torch.bmm(torch.transpose(X, 1, 2), input_transform).transpose(1, 2)
+
+        # Generate feature vector of the transformed cloud.
+        xb = F.relu(self.bn1(self.conv1(xb)))
+
+        # Transform the feature vector.
+        feature_transform = self.t_net_feature(xb)
+        xb = torch.bmm(torch.transpose(xb, 1, 2), feature_transform).transpose(1, 2)
+        
+        # Expand the feature vector & apply symmetric function.
+        xb = F.relu(self.bn2(self.conv2(xb)))
+        xb = self.bn3(self.conv3(xb))
+        xb = torch.nn.MaxPool1d(xb.size(-1))(xb)
+        # Generate global feature vector.
+        output = torch.nn.Flatten(1)(xb)
+
+        return output, input_transform, feature_transform
+
+class PointNetClassification(torch.nn.Module):
+    """PointNet Classification model.
+    It extends on the output from the Transform network, adding fully  connected layers that
+    produce final output scores (which are given as probabilities). 
+
+    Source: Qi, Charles R. et. al. - PointNet Deep Learning on Point Sets for 
+    3D Classification and Segmentation
+    """
+    def __init__(self, classes: int = 10):
+        """
+        Args:
+            classes: Number of classes that span possible predictions.
+        """
+        super().__init__()
+        self.transform = Transform()
+        self.classes = classes
+        # Define fully connected layers.
+        self.fc1 = torch.nn.Linear(1024, 512)
+        self.fc2 = torch.nn.Linear(512, 256)
+        self.fc3 = torch.nn.Linear(256, self.classes)
+        # Define batch normalizations.
+        self.bn1 = torch.nn.BatchNorm1d(512)
+        self.bn2 = torch.nn.BatchNorm1d(256)
+        
+        # Define droput rate & logarthimg softmax transformation
+        # to achieve output whose elements lie in range [0, 1] and 
+        # sum up to 1.
+        self.dropout = torch.nn.Dropout(p=0.3)
+        self.logsoftmax = torch.nn.LogSoftmax(dim=1)
+
+    def forward(self, X: torch.tensor) -> torch.tensor:
+        """Defines 1 forward pass of the PointNet classification 
+        network.
+
+        Args:
+            X: Input data.
+
+        Returns:
+            Global feature vector and transformation matrices.
+        """    
+        xb, input_transform, feature_transform = self.transform(X)
+        xb = F.relu(self.bn1(self.fc1(xb)))
+        xb = F.relu(self.bn2(self.dropout(self.fc2(xb))))
+        output = self.fc3(xb)
+
+        return self.logsoftmax(output), input_transform, feature_transform
+
+def point_net_loss(outputs, labels, input_transform, feature_transform, alpha = 0.0001):
+    """Loss computation of the PointNet Classification model. Loss is based on the negative
+    likelihood loss computation, which is common for classificiation problems. Regularization
+    term is added to it, which constrains transformation matrices to be close to orthogonal
+    matrix - to minimize the difficulty of their optimization.  
+
+    # TODO(vice) Check types!
+    Args:
+        outputs: Predicted labels.
+        labels: Ground truth labels.
+        input_transform: Input point cloud transformation.
+        feature_transform: Feature vector transformation.
+        alpha: Regularization term weight.
+
+    Returns:
+        Compute loss.
+    """
+    criterion = torch.nn.NLLLoss() # Negative likelihood loss.
+
+    # Initialize identity transformations.
+    batch_size = outputs.size(0)
+    id_input_transform = torch.eye(3, requires_grad=True).repeat(batch_size, 1, 1)
+    id_feature_transform = torch.eye(64, requires_grad=True).repeat(batch_size, 1, 1)
+
+    if outputs.is_cuda:
+        id_input_transform = id_input_transform.cuda()
+        id_feature_transform = id_feature_transform.cuda()
+    
+    # Compute transformation regularization term (Lreg from the paper).
+    l_reg_input = id_input_transform - torch.bmm(input_transform,
+                                                        input_transform.transpose(1, 2))
+    l_reg_feature = id_feature_transform - torch.bmm(feature_transform,
+                                                            feature_transform.transpose(1, 2))
+    
+    loss = criterion(outputs, labels) + alpha *\
+        (torch.norm(l_reg_input)+torch.norm(l_reg_feature)) / float(batch_size)
+
+    return loss
